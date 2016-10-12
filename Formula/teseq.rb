88fb9d048e57ff83e1aa8a5dd60396febc0844d1
George Hartzell <george.hartzell.gh1@contractors.roche.com>
2016-10-11 17:05:30 -0700

    First draft formula for teseq
    Autogenerated by brew for me....

---
+++ Formula/teseq.rb
@@ -0,0 +1,37 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Teseq < Formula
+  desc ""
+  homepage ""
+  url "https://ftp.gnu.org/gnu/teseq/teseq-1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "32fbd22bc1e16796a02a3915ac6c29015607a19b00a50de570c747860b009622"
+
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test teseq`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
